d0812a0bd18dd0cda72b7a732145d841fc4bef23
Franc6220 <samuelwangalibo@yahoo.com>
2024-01-03 08:40:27 -0800

    Positive

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-# YOUR CODE HERE
-#Ask user to enter number
-number = input("Enter any number ")
-
 #If number is greater than zero
 if number > 0
    print("{} is positive".format(number))
---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+#if last digit is greater than five
+if 

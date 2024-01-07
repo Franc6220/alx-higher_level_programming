60a1afa7d3f5f4d0693c1e1dc5ca1e9d42ce62c4
Franc6220 <samuelwangalibo@yahoo.com>
2024-01-07 06:42:55 -0800

    Copy - Cut - Paste

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+#Print First 3 letters
+print(f"First 3 letters: {word[:3]}")
+#Print Last 2 letters
+print(f"Last 2 letters: {word[-2:]}")
+#Print Middle Word
+print(f"Middle word: {word[2:8}")

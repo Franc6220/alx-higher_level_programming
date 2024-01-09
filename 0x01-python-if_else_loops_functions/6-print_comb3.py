5b9ddc472aeccf808e8bf917c404f6681a03f1f6
Franc6220 <samuelwangalibo@yahoo.com>
2024-01-09 07:30:10 -0800

    Comb3

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for first_integer in range(0, 10):
+    for second_integer in range(first_integer + 1, 10):
+        print("{}{}".format(first_integer, second_integer))
+        print("{}{}".format(first_integer, second_integer))     

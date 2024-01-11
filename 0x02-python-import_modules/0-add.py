6836ba38c65320ae175b0117e23e151f524bf00e
Franc6220 <samuelwangalibo@yahoo.com>
2024-01-11 11:48:36 -0800

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def add(a, b):
+if __name__ == "__main__":
+    from add_o import add
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add_0)
+    print("{} + {} = {}".format(a, b, add(a, b)))

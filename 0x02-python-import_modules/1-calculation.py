87f4587020f7d166b62ec23d935141bef2704614
Franc6220 <samuelwangalibo@yahoo.com>
2024-01-16 07:48:28 -0800

    calculation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 if _name_ == "_main_":
+   """print math calculation of a and b"""
    from calculator_1 import ccalculator
+        
         a = 10
         b = 5
+
         print("{} + {} = {}".format(a, b, add(a, b)))
         print("{} - {} = {}".format(a, b, sub(a, b)))
         print("{} * {} = {}".format(a, b, mul(a, b)))

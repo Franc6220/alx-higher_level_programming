354334e622b3b78fb0c24d082ad9a70f08bf02f6
Franc6220 <samuelwangalibo@yahoo.com>
2024-01-16 21:18:25 -0800

    calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,11 +2,17 @@
 if _name_ == "_main_":
    """print math calculation of a and b"""
    from calculator_1 import ccalculator
-        
         a = 10
         b = 5
-
-        print("{} + {} = {}".format(a, b, add(a, b)))
-        print("{} - {} = {}".format(a, b, sub(a, b)))
-        print("{} * {} = {}".format(a, b, mul(a, b)))
-        print("{} / {} = {}".format(a, b, div(a, b)))
+        if def add(a, b):
+           print("{} + {} = {}".format(a, b, add(a, b)))
+        return (a + b)
+        elif def sub(a, b):
+             print("{} - {} = {}".format(a, b, sub(a, b)))
+        return (a - b)
+        elif def mul(a, b):
+             print("{} * {} = {}".format(a, b, mul(a, b)))
+        return (a * b)
+        elif def div(a, b):
+             print("{} / {} = {}".format(a, b, div(a, b)))
+        return int(a / b)

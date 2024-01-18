e6fa33f40a155a0bed5c4f8f5de4166e8594acae
Franc6220 <samuelwangalibo@yahoo.com>
2024-01-18 11:17:50 -0800

    reversed

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     if my_list:
          my_list.reverse()
-         for integer in my_list:
-             print(df['my_list'].to_csv(index=False))
+         for number in my_list:
+             print("{:d}".format(number))

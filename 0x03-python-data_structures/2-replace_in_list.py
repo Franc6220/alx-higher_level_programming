3200d31e4664be60694de02bd2056f4ce1d55e64
Franc6220 <samuelwangalibo@yahoo.com>
2024-01-18 10:44:42 -0800

    replace

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    [[row[my_list] for row in matrix] for my_list in range(4)]
+    listlength = len(my_list) - 1    #Since my_list is from 0 to 5 this enables it to be equals to listlength after this 
+    if (idx < 0 and idx > listlength)
+        return (my_list)
+    elif (idx > 0 and idx < listlength)
+        return (new_list)
+    else:
+         mylist[idx] = element
+         return (my_list)

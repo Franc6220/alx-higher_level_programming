6be9756c250711a7f1351448caa7dcb5c292f80a
Franc6220 <samuelwangalibo@yahoo.com>
2024-01-30 02:09:14 -0800

    Simple rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-"""0-rectangle folder for empty class """
+
 
 class Rectangle:
-""" Empty class that defines a rectangle """
-
-      pass       #An empty block
-
-
-r = Rectangle()
-
-print(r)
+      pass

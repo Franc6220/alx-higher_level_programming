b05980ab8d0687a387eeaa69ada06e1b0f48aa0b
Franc6220 <samuelwangalibo@yahoo.com>
2024-06-14 09:56:10 -0700

    relationship

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-# Lists all States and corresponding Cities in the database hbtn_0e_101_usa.
-# Usage: ./101-relationship_states_cities_list.py <mysql username> /
-#                                                 <mysql password> /
-#                                                 <database name>
+# 101-relationship_states_cities_list.py
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -10,13 +8,27 @@ from relationship_state import State
 from relationship_city import City
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    # Read command line arguments
+    username, password, dbname = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    # Create engine to connect to MySQL database
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{dbname}')
+
+    # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
+
+    # Create a session
     session = Session()
 
-    for state in session.query(State).order_by(State.id):
-        print("{}: {}".format(state.id, state.name))
+    # Query all State objects and corresponding City objects
+    states = session.query(State).order_by(State.id).all()
+
+    # Print the results
+    for state in states:
+        print(f"{state.id}: {state.name}")
         for city in state.cities:
-            print("    {}: {}".format(city.id, city.name))
+            print(f"\t{city.id}: {city.name}")
+
+    # Close the session
+    session.close()
+

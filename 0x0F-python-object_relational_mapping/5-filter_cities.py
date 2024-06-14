1ad342b816bd080711714334a899a8e656fa5a99
Franc6220 <samuelwangalibo@yahoo.com>
2024-06-14 07:11:02 -0700

    filter-cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,19 +1,60 @@
 #!/usr/bin/python3
-# Displays all cities of a given state from the
-# states table of the database hbtn_0e_4_usa.
-# Safe from SQL injections.
-# Usage: ./5-filter_cities.py <mysql username> \
-#                             <mysql password> \
-#                             <database name> \
-#                             <state name searched>
+"""
+5-filter_cities module
+This script lists all cities of a given state from the database hbtn_0e_4_usa.
+"""
+
 import sys
 import MySQLdb
 
+def filter_cities_by_state():
+    """
+    Connects to the MySQL database and lists all cities of the given state,
+    sorted by cities.id.
+    """
+    # Retrieve command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to the MySQL database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
+
+    # Create a cursor object
+    cursor = db.cursor()
+
+    # Execute the query to retrieve cities of the given state
+    query = """
+            SELECT cities.name
+            FROM cities
+            JOIN states ON cities.state_id = states.id
+            WHERE states.name = %s
+            ORDER BY cities.id ASC
+            """
+
+    cursor.execute(query, (state_name,))
+
+    # Fetch all the rows
+    rows = cursor.fetchall()
+
+    # Extract city names from the result set
+    cities = [row[0] for row in rows]
+
+    # Print the cities as a comma-separated string
+    if cities:
+        print(", ".join(cities))
+
+    # Close the cursor and the database connection
+    cursor.close()
+    db.close()
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                   ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
+    filter_cities_by_state()
+

084e2b93da37b5cd04aebfbfcdc3e36d50f81324
Franc6220 <samuelwangalibo@yahoo.com>
2024-06-14 07:42:29 -0700

    fetch

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,22 +1,40 @@
 #!/usr/bin/python3
-# Prints the first State object from the database hbtn_0e_6_usa.
-# Usage: ./8-model_state_fetch_first.py <mysql username> /
-#                                       <mysql password> /
-#                                       <database name>
+"""
+8-model_state_fetch_first module
+This script retrieves the first State object from the database hbtn_0e_6_usa.
+"""
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    # Retrieve command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
-    state = session.query(State).order_by(State.id).first()
-    if state is None:
+    # Create engine to connect to MySQL server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(mysql_username, mysql_password, database_name),
+                           pool_pre_ping=True)
+
+    # Bind the engine to the Base class
+    Base.metadata.bind = engine
+
+    # Create a session
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
+    # Query the first State object and order by id
+    first_state = session.query(State).order_by(State.id).first()
+
+    if first_state is None:
         print("Nothing")
     else:
-        print("{}: {}".format(state.id, state.name))
+        print(f"{first_state.id}: {first_state.name}")
+
+    # Close the session
+    session.close()
+

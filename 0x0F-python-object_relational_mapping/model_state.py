7405d4e95985946b4aadb016a25629f173d304d1
Franc6220 <samuelwangalibo@yahoo.com>
2024-06-14 07:19:46 -0700

    model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,20 +1,35 @@
 #!/usr/bin/python3
-# Defines a State model.
-# Inherits from SQLAlchemy Base and links to the MySQL table states.
+"""
+6-model_state module
+This script defines the State class and initializes the database.
+"""
 
-from sqlalchemy import Column, Integer, String
+import sys
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
+# Create declarative_base instance
 Base = declarative_base()
 
-
 class State(Base):
-    """Represents a state for a MySQL database.
-
-    __tablename__ (str): The name of the MySQL table to store States.
-    id (sqlalchemy.Integer): The state's id.
-    name (sqlalchemy.String): The state's name.
     """
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True)
+    State class that represents a state table in a MySQL database
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
+
+if __name__ == "__main__":
+    # Create an engine that connects to the MySQL server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    # Import all tables that inherit from Base
+    from sqlalchemy.orm import sessionmaker
+    Session = sessionmaker(bind=engine)
+
+    # Create all tables in the database if they do not exist
+    Base.metadata.create_all(engine)
+

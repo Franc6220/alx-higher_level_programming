50569ed4734b1e05c5c511828318551eceef12c2
Franc6220 <samuelwangalibo@yahoo.com>
2024-06-14 08:53:48 -0700

    insert

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,21 +1,44 @@
 #!/usr/bin/python3
-# Adds the State object "Louisiana" to the database hbtn_0e_6_usa.
-# Usage: ./11-model_state_insert.py <mysql username> /
-#                                   <mysql password> /
-#                                   <database name>
+"""
+11-model_state_insert module
+This script adds a State object "Louisiana" to the database hbtn_0e_6_usa and prints its id after creation.
+"""
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    # Retrieve command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
-    louisiana = State(name="Louisiana")
-    session.add(louisiana)
+    # Create engine to connect to MySQL server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(mysql_username, mysql_password, database_name),
+                           pool_pre_ping=True)
+
+    # Bind the engine to the Base class
+    Base.metadata.bind = engine
+
+    # Create a session
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
+    # Create a new State object
+    new_state = State(name='Louisiana')
+
+    # Add the new state to the session
+    session.add(new_state)
+
+    # Commit the session to save the new state to the database
     session.commit()
-    print(louisiana.id)
+
+    # Print the id of the newly added state
+    print(new_state.id)
+
+    # Close the session
+    session.close()
+
